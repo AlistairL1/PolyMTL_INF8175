d05aacce2d31a3cabc253edc72a4766cd9a87a4c
alistair <alistair.lucet@gmail.com>
2024-11-04 12:06:15 -0500

    Implementation solution Partie 1 MAIS probleme pour la gestion du temps (4eme instance)

--- Devoir2_INF8175_A24/RechercheLocale/code/solver_advanced.py
+++ Devoir2_INF8175_A24/RechercheLocale/code/solver_advanced.py
@@ -1,11 +1,40 @@
-
+import numpy as np
 
 def solve(schedule):
     """
-    Your solution of the problem
-    :param schedule: object describing the input
-    :return: a list of tuples of the form (c,t) where c is a course and t a time slot. 
+    Optimized solution using a local search algorithm to minimize conflicts.
+    :param schedule: object describing the input (with courses and conflicts)
+    :return: a dictionary of the form {course: time} where course is a course and time a time slot.
     """
-    # Add here your agent
+    # Initialisation : assigner chaque cours au créneau initial 0
+    courses = schedule.course_list
+    conflicts = schedule.conflict_list
+    max_time_slots = len(courses) // 2
+    solution = {course: 0 for course in courses}
 
-    raise Exception("Agent is not implemented")
+    # Fonction pour calculer le nombre de conflits
+    def nb_conflits(solution):
+        return sum(1 for course1, course2 in conflicts if solution[course1] == solution[course2])
+
+    nb_conflits_actuel = nb_conflits(solution)
+    amelioration = True
+
+    while amelioration and nb_conflits_actuel > 0:
+        amelioration = False
+        for course in courses:
+            creneau_actuel = solution[course]
+            nb_conflits_tempo = nb_conflits_actuel
+            for nouveau_creneau in range(max_time_slots):  # Essayer d'autres créneaux
+                if nouveau_creneau != creneau_actuel:
+                    solution[course] = nouveau_creneau
+                    noubeau_nb_conflits = nb_conflits(solution)
+                    if noubeau_nb_conflits < nb_conflits_tempo:
+                        creneau_actuel = nouveau_creneau
+                        nb_conflits_tempo = noubeau_nb_conflits
+                        amelioration = True
+                    # Pas besoin de revenir à l'état initial ici, car on applique directement le meilleur créneau
+            solution[course] = creneau_actuel  # Appliquer le meilleur créneau trouvé
+        nb_conflits_actuel = nb_conflits(solution)
+
+    return solution
+
